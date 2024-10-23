011c2cbe0f7a3fdd53a776aae3692661c2fc4fda
Bruno <brunohoffmann.2003@gmail.com>
2024-10-23 16:50:12 -0300

    Melhora a implementação da BST

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -2,179 +2,203 @@
 #include <stdlib.h>
 
 typedef struct node{
-    int valor;
+    int valor;     
     int quantidade;
-    struct node *maior;
-    struct node *menor;
+    struct node *esquerdo;
+    struct node *direito;
 } node;
-
-void menu(){
-    printf("\n");
-    printf("1\tInserir um elemento\n");
-    printf("2\tBuscar um elemento\n");
-    printf("3\tExibir elementos em ordem\n");
-    printf("4\tRemover um elemento\n");
-    printf("5\tMostrar a altura da arvore\n");
-    printf("6\tEncerrar\n");
-    printf("Escolha: ");
-}
 
 node *cria_node(int valor){
     node *temp = malloc(sizeof(node));
-    temp->maior = NULL;
-    temp->menor = NULL;
     temp->valor = valor;
     temp->quantidade = 1;
-    
+    temp->direito = NULL;
+    temp->esquerdo = NULL;
+
     return temp;
 }
 
-void adiciona_node(node *novo, node **posicao_atual){
-    if(*posicao_atual == NULL){
-        *posicao_atual = novo;
-    }
-    else if((*posicao_atual)->valor == novo->valor){
-        (*posicao_atual)->quantidade += 1;
-    }
-    else if(novo->valor < (*posicao_atual)->valor){
-        adiciona_node(novo, &(*posicao_atual)->menor);
-    }
-    else if(novo->valor > (*posicao_atual)->valor){
-        adiciona_node(novo, &(*posicao_atual)->maior);
-    }
-}
-
-void printa_em_ordem(node *node_printado){
-    if(node_printado == NULL){
+void adiciona_node(node *novo, node **atual){
+    if(*atual == NULL){
+        *atual = novo;
         return;
     }
-    printa_em_ordem(node_printado->menor);
-    printf("%i(%i) ", node_printado->valor, node_printado->quantidade);
-    printa_em_ordem(node_printado->maior);
+    if(novo->valor > (*atual)->valor){
+        adiciona_node(novo, &(*atual)->direito);
+        return;
+    }
+    if(novo->valor < (*atual)->valor){
+        adiciona_node(novo, &(*atual)->esquerdo);
+        return;
+    }
+    if(novo->valor == (*atual)->valor){
+        (*atual)->quantidade += 1;
+        return;
+    }
 }
 
-int busca_elemento(node *node_atual, int valor){
-    if(node_atual == NULL){
+void printa_infixo(node *atual){
+    if(atual == NULL){
+        return;
+    }
+    printa_infixo(atual->esquerdo);
+    printf("%i", atual->valor);
+    if(atual->quantidade > 1){
+        printf("(%i)", atual->quantidade);
+    }
+    printf(" ");
+    printa_infixo(atual->direito);
+}
+
+void printa_prefixo(node *atual){
+    if(atual == NULL){
+        return;
+    }
+    printf("%i", atual->valor);
+    if(atual->quantidade > 1){
+        printf("(%i)", atual->quantidade);
+    }
+    printf(" ");
+    printa_prefixo(atual->esquerdo);
+    printa_prefixo(atual->direito);
+}
+
+void printa_posfixo(node *atual){
+    if(atual == NULL){
+        return;
+    }
+    printa_posfixo(atual->esquerdo);
+    printa_posfixo(atual->direito);
+    printf("%i", atual->valor);
+    if(atual->quantidade > 1){
+        printf("(%i)", atual->quantidade);
+    }
+    printf(" ");
+}
+
+int busca_valor(int valor, node *atual){
+    if(atual == NULL){
         return 0;
     }
-    if(node_atual->valor == valor){
+    if(atual->valor == valor){
         return 1;
     }
-    else if(valor < node_atual->valor){
-        return busca_elemento(node_atual->menor, valor);
+    if(valor < atual->valor){
+        return busca_valor(valor, atual->esquerdo);
     }
-    else if(valor > node_atual->valor){
-        return busca_elemento(node_atual->maior, valor);
+    if(valor > atual->valor){
+        return busca_valor(valor, atual->direito);
     }
     return 0;
 }
 
-int calcula_altura(node *node_atual){
-    if(node_atual == NULL){
-        return -1;
-    }
-     int altura_esquerda = calcula_altura(node_atual->menor);
-     int altura_direita = calcula_altura(node_atual->maior);
-
-     if(altura_esquerda > altura_direita){
-        return altura_esquerda + 1;
-     }
-     else{
-        return altura_direita + 1;
-     }
-}
-
-node **encontra_predecessor(node **node_atual){
-    if((*node_atual)->maior == NULL){
-        return node_atual;
-    }
-    encontra_predecessor(&(*node_atual)->maior);
-    return 0;
-}
-
-int remove_node(int valor, node **node_atual){
-    if(*node_atual == NULL){
+int conta_nodes(node *atual){
+    if(atual == NULL){
         return 0;
     }
-    if((*node_atual)->valor == valor){
-        if((*node_atual)->menor == NULL && (*node_atual)->maior == NULL){
-            *node_atual = NULL;
-            return 1;
-        }
-        if((*node_atual)->menor != NULL && (*node_atual)->maior == NULL){
-            *node_atual = (*node_atual)->menor;
-            return 1;
-        }
-        if((*node_atual)->maior != NULL && (*node_atual)->menor == NULL){
-            *node_atual = (*node_atual)->maior;
-            return 1;
-        }
-        if((*node_atual)->maior != NULL && (*node_atual)->menor != NULL){
-            node **temp = encontra_predecessor(&(*node_atual)->maior);
-            (*node_atual)->valor = (*temp)->valor;
-            *temp = (*temp)->menor;
-            return 1;
-        }
+    int esquerda = conta_nodes(atual->esquerdo);
+    int direita = conta_nodes(atual->direito);
+    return direita + esquerda + 1;
+}
+
+int conta_altura(node *atual){
+    if(atual == NULL){
+        return -1;
     }
-    else if(valor < (*node_atual)->valor){
-        return remove_node(valor, &(*node_atual)->menor);
+    int esquerda = conta_altura(atual->esquerdo);
+    int direita = conta_altura(atual->direito);
+    if(esquerda > direita){
+        return esquerda + 1;
     }
-    else if(valor > (*node_atual)->valor){
-        return remove_node(valor, &(*node_atual)->maior);
+    else{
+        return direita + 1;
+    }
+}
+
+int encontra_predecessor(node **atual){
+    if((*atual)->direito == NULL){ // Está no predecessor.
+        int valor_predecessor = (*atual)->valor;
+        node *temp = *atual;
+        if((*atual)->esquerdo == NULL){
+            *atual = NULL;
+        }
+        else{
+            *atual = (*atual)->esquerdo;
+        }
+        free(temp);
+        return valor_predecessor;
+    }
+    else{ // Não está no predecessor.
+        return encontra_predecessor(&(*atual)->direito);
     }
     return 0;
+}
+
+void deleta_node(node **atual, int valor){
+    if(*atual == NULL){
+        return;
+    }
+    if(valor < (*atual)->valor){
+        deleta_node(&(*atual)->esquerdo, valor);
+        return;
+    }
+    if(valor > (*atual)->valor){
+        deleta_node(&(*atual)->direito, valor);
+        return;
+    }
+    if(valor == (*atual)->valor){
+        // Mais de um do valor
+        if((*atual)->quantidade > 1){
+            (*atual)->quantidade -= 1;
+            return;
+        }
+        node *temp = *atual; // Guarda node que será liberado.
+
+        // Node tem 1 ou 0 filhos
+        if((*atual)->esquerdo == NULL){
+            *atual = (*atual)->direito;
+            free(temp);
+        }
+        else if((*atual)->direito == NULL){
+            *atual = (*atual)->esquerdo;
+            free(temp);
+        }
+        // Nó tem 2 filhos
+        else{
+            (*atual)->valor = encontra_predecessor(&(*atual)->esquerdo);
+        }
+
+    }
+
 }
 
 int main(){
     node *raiz = NULL;
-    int opcao;
-    while(opcao != 6){
-        menu();
-        scanf("%i", &opcao);
-        while(getchar() != '\n');
-        printf("\n");
 
-        switch(opcao){
-            case 1:{
-                int valor;
-                printf("Insira o valor adicionado: ");
-                scanf("%i", &valor);
-                while(getchar() != '\n');
-                adiciona_node(cria_node(valor), &raiz);
-                break;
-            }
-
-            case 2:{
-                int valor;
-                printf("Insira o valor buscado: ");
-                scanf("%i", &valor);
-                while(getchar() != '\n');
-                printf("Retorno : %i\n", busca_elemento(raiz, valor));
-                break;
-            }
-
-            case 3:{
-                printa_em_ordem(raiz);
-                break;
-            }
-
-            case 4:{
-                int valor;
-                printf("Insira o valor a ser removido: ");
-                scanf("%i", &valor);
-                while(getchar() != '\n');
-                printf("Retorno: %i\n", remove_node(valor, &raiz));
-                break;
-            }
-
-            case 5:{
-                    int altura = calcula_altura(raiz);
-                    printf("Altura: %i\n", altura);
-                break;
-            }
-
-        }
-    }
+    adiciona_node(cria_node(5), &raiz);
+    adiciona_node(cria_node(10), &raiz);
+    adiciona_node(cria_node(5), &raiz);
+    adiciona_node(cria_node(4), &raiz);
+    printa_posfixo(raiz);
+    printf("\n");
+    printf("%i\n", busca_valor(5, raiz));
+    printf("%i\n", busca_valor(4, raiz));
+    printf("%i\n", busca_valor(10, raiz));
+    printf("%i\n", busca_valor(3, raiz));
+    printf("%i\n", busca_valor(7, raiz));
+    printf("%i\n", conta_nodes(raiz));
+    printf("%i\n", conta_altura(raiz));
+    adiciona_node(cria_node(7), &raiz);
+    printf("%i\n", conta_altura(raiz));
+    printa_infixo(raiz);
+    printf("\n");
+    deleta_node(&raiz, 5);
+    deleta_node(&raiz, 5);
+    printa_infixo(raiz);
+    printf("\n");
+    deleta_node(&raiz, 7);
+    printa_infixo(raiz);
+    printf("\n");
 
     return 0;
 }
